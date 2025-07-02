8dddd72975f132aa57a3d7cc70ffdfab5feab3a2
Gautam Mishra <129742564+gautamm2707@users.noreply.github.com>
2025-07-02 20:20:32 +0530

    Update main.tf

--- terraform/vcn/main.tf
+++ terraform/vcn/main.tf
@@ -1,3 +1,13 @@
+terraform {
+  
+  backend "oci" {
+    bucket = "bucket-tag"
+    key = "tf_demo_native_backend.tfstate"
+    workspace_key_prefix = "demo/"
+    namespace = "id3kvohtwgjy"
+
+  }
+}
 provider "oci" {
   region               = var.region
   tenancy_ocid         = var.tenancy_ocid
@@ -9,15 +19,4 @@ resource "oci_core_virtual_network" "fvass_vcn" {
   cidr_block     = var.vcn_cidr_block
   compartment_id = var.compartment_ocid
   display_name   = var.vcn_display_name
-}
-
-terraform {
-  
-  backend "oci" {
-    bucket = "bucket-tag"
-    key = "tf_demo_native_backend.tfstate"
-    workspace_key_prefix = "demo/"
-    namespace = "id3kvohtwgjy"
-
-  }
 }
