578fe1fb3d705a6680540f9ef441834fa06b1c5a
Gautam Mishra <129742564+gautamm2707@users.noreply.github.com>
2025-07-03 18:55:57 +0530

    Update main.tf

--- terraform/vcn/main.tf
+++ terraform/vcn/main.tf
@@ -1,11 +1,22 @@
 terraform {
   
-   backend "oci" {
+  backend "s3" {
     bucket = "bucket-tag"
+    region = "us-ashburn-1"
     key = "tf_demo_native_backend.tfstate"
-    namespace = "id3kvohtwgjy"
+    use_lockfile = true
+    
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+    skip_metadata_api_check     = true
+    use_path_style              = true
+
+    endpoints = { s3 = "https://id3kvohtwgjy.compat.objectstorage.us-ashburn-1.oci-customer.oci.com" } 
   }
 }
+
 provider "oci" {
   region               = var.region
   tenancy_ocid         = var.tenancy_ocid
