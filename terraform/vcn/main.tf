2cfcd1a3a770da1b1cb37a35c0615cca044881e9
Gautam Mishra <129742564+gautamm2707@users.noreply.github.com>
2025-07-03 18:01:53 +0530

    Update main.tf

--- terraform/vcn/main.tf
+++ terraform/vcn/main.tf
@@ -10,7 +10,7 @@ provider "oci" {
   region               = var.region
   tenancy_ocid         = var.tenancy_ocid
   auth                 = "securitytoken"
-  config_file_profile  = "upst"
+  config_file_profile  = "DEFAULT"
 }
 
 resource "oci_core_virtual_network" "fvass_vcn" {
